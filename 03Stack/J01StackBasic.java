5d71242be7af44d1f22375a9c530cbab05aad937
khushikri1209 <khushikumari12029@gmail.com>
2025-04-02 21:04:02 +0530

    Create J01StackBasic.java

---
+++ 03Stack/J01StackBasic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class J01StackBasic {
+
+    public static void main(String args[]) {
+        // stack s=new stack<>();
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+
+        }
+    }
+
+}
