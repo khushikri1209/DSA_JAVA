c29ea20196e2fc600aa30dfa096ac956392ee26b
khushikri1209 <khushikumari12029@gmail.com>
2025-04-27 20:02:16 +0530

    Create J07RemoveAllQueue.java

---
+++ 06Queue/J07RemoveAllQueue.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class J07RemoveAllQueue {
+    public static void main(String[] args) {
+        Queue<String> queue_list = new LinkedList<>();
+        queue_list.add("Yellow");
+        queue_list.add("Green");
+        queue_list.add("Pink");
+        queue_list.add("Black");
+        queue_list.add("Blue");
+        queue_list.add("White");
+
+        System.out.println("Queue Elements Before Clearing : " + queue_list);
+
+        // Clear the queue
+        queue_list.clear();
+
+        System.out.println("Queue Elements After Clearing : " + queue_list);
+    }
+}
