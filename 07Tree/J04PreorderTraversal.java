4f9ba68f20b81122af1a2724d545772d7e41c1fe
khushikri1209 <khushikumari12029@gmail.com>
2025-06-01 13:32:43 +0530

    Create J04PreorderTraversal.java

---
+++ 07Tree/J04PreorderTraversal.java
@@ -0,0 +1,33 @@
+public class J04PreorderTraversal {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int value) {
+            data = value;
+            left = right = null;
+        }
+    }
+
+    // Preorder Traversal: Root -> Left -> Right
+    void preorder(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        J04PreorderTraversal tree = new J04PreorderTraversal();
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Preorder traversal of binary tree:");
+        tree.preorder(root);
+    }
+}
