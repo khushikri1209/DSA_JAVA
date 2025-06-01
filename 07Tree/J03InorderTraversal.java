3d6b9fd5573838ee8387d83c6c8ddfa0aa10cd0e
khushikri1209 <khushikumari12029@gmail.com>
2025-06-01 13:28:47 +0530

    Create J03InorderTraversal.java

---
+++ 07Tree/J03InorderTraversal.java
@@ -0,0 +1,33 @@
+public class J03InorderTraversal {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int value) {
+            data = value;
+            left = right = null;
+        }
+    }
+
+    // Inorder Traversal: Left -> Root -> Right
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        J03InorderTraversal tree = new J03InorderTraversal();
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Inorder traversal of binary tree:");
+        tree.inorder(root);
+    }
+}
